42f098cb6dccb61f2b301c22bd501f97e056bfbf
Beymar <beymarchoque1522003@gmail.com>
2026-03-15 21:58:31 -0300

    Ejercicio3 terminado

---
+++ ejercicio_3.py
@@ -0,0 +1,16 @@
+curso = "Python para iniciantes"
+print(curso.upper())
+print(curso)
+print(curso.lower())
+print(curso.capitalize())
+
+#busquedas
+print(curso.find("h"))
+print(curso.find("cia"))
+
+#reemplazos
+print(curso.replace("para","FOR"))
+print(curso)
+
+#Operador
+print("para" in curso)
