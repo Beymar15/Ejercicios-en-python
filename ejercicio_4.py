ba59cb7423e988ce9366a43d49c3af5ffa40006a
Beymar <beymarchoque1522003@gmail.com>
2026-03-15 22:07:46 -0300

    Ejercicio4 terminado

---
+++ ejercicio_4.py
@@ -0,0 +1,25 @@
+#Operadores matematicos
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 4)
+
+#Division entera
+print(10 // 4)
+#Modulo o residuo
+print(10 % 3)
+#Exponente
+print(2 ** 4)
+
+
+x = 10
+x = x + 5
+print(x)
+
+#Operador de asignacion compacto
+y = 20
+y *= 5
+print(y)
+# Procedencia
+x = 10 + (3 * 2)
+print(x)
