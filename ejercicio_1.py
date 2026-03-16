4dc1cefb2bf0638706fa0df792f2c7d47ed5d7f8
Beymar <beymarchoque1522003@gmail.com>
2026-03-15 21:33:15 -0300

    Ejercicio1 terminado

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,1 +1,17 @@
 print("Hola mundo desde python!!")
+#Variables en python
+#Enteros
+edad = 20
+#Float
+precio = 50,5
+#Str
+nombre = "Bruno Diaz"
+#Bool
+bandera = True
+#Salida
+print("Nombre: ",nombre)
+print("Edad: ",edad)
+print("Precio: "+str(precio))
+#Entrada
+nombre = input("Introduce tu nombre: ")
+print("Hola",nombre)
